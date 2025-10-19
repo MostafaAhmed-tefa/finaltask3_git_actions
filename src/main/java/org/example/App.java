508b7a36ad0c5a16d79f0aa015683fd4a5112722
mosta <mostafaahmed49k@gmail.com>
2025-10-19 13:47:19 +0300

    final commit

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,7 +1,16 @@
 package org.example;
 
 public class App {
+
     public int add(int a, int b) {
-        return a + b+3;
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
     }
 }
--- src/test/java/SimpleAppTest.java
+++ src/test/java/SimpleAppTest.java
@@ -3,9 +3,22 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class SimpleAppTest {
+
     @Test
     void testAdd() {
         App app = new App();
-        assertEquals(5, app.add(2, 3));
+        assertEquals(15, app.add(7, 8));
+    }
+
+    @Test
+    void testSubtract() {
+        App app = new App();
+        assertEquals(4, app.subtract(10, 6));
+    }
+
+    @Test
+    void testMultiply() {
+        App app = new App();
+        assertEquals(42, app.multiply(6, 7));
     }
 }
